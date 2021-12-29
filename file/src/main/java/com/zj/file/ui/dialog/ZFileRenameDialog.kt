8a6839b2801664d4de59f59784075fb3b08ef2db
zhujiang2 <zhujiang2@lenovo.com>
2021-12-29 21:11:40 +0800

    使用Ime来适配输入法弹出隐藏

--- file/src/main/java/com/zj/file/ui/dialog/ZFileRenameDialog.kt
+++ file/src/main/java/com/zj/file/ui/dialog/ZFileRenameDialog.kt
@@ -1,30 +1,26 @@
 package com.zj.file.ui.dialog
 
-import android.app.Activity
 import android.app.Dialog
-import android.content.Context
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
-import android.view.inputmethod.InputMethodManager
 import com.zj.file.R
 import com.zj.file.common.ZFileManageDialog
+import com.zj.file.content.hideIme
 import com.zj.file.content.isNull
 import com.zj.file.content.setNeedWH
+import com.zj.file.content.showIme
 import com.zj.file.databinding.DialogZfileRenameBinding
 import com.zj.file.util.ZFileLog
 import com.zj.file.util.showToast
 
-internal class ZFileRenameDialog : ZFileManageDialog(), Runnable {
+internal class ZFileRenameDialog : ZFileManageDialog() {
 
     private var binding: DialogZfileRenameBinding? = null
     var reanameDown: (String.() -> Unit)? = null
-    private var handler: Handler? = null
     private var oldName = "请输入文件名称"
 
     companion object {
@@ -50,7 +46,6 @@ internal class ZFileRenameDialog : ZFileManageDialog(), Runnable {
 
     override fun init(savedInstanceState: Bundle?) {
         oldName = arguments?.getString("oldName") ?: "请输入文件名称"
-        handler = Handler(Looper.getMainLooper())
         binding?.zfileDialogRenameEdit?.setOnEditorActionListener { _, actionId, _ ->
             if (actionId == EditorInfo.IME_ACTION_DONE || actionId == EditorInfo.IME_ACTION_UNSPECIFIED) {
                 rename()
@@ -84,31 +79,12 @@ internal class ZFileRenameDialog : ZFileManageDialog(), Runnable {
     override fun onStart() {
         super.onStart()
         setNeedWH()
-        handler?.postDelayed(this, 150)
-    }
-
-    override fun run() {
-        binding?.zfileDialogRenameEdit?.requestFocus()
-        val imm = context?.getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
-        imm?.showSoftInput(binding?.zfileDialogRenameEdit, InputMethodManager.SHOW_IMPLICIT)
+        binding?.zfileDialogRenameEdit?.showIme()
     }
 
     override fun onDestroyView() {
-        closeKeyboard()
-        handler?.removeCallbacks(this)
-        handler?.removeCallbacksAndMessages(null)
-        handler = null
+        binding?.zfileDialogRenameEdit?.hideIme()
         super.onDestroyView()
     }
 
-    private fun closeKeyboard() {
-        val m = context?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        val isOpen = m.isActive
-        if (isOpen) {
-            m.hideSoftInputFromWindow(
-                (context as Activity).currentFocus?.windowToken,
-                InputMethodManager.HIDE_NOT_ALWAYS
-            )
-        }
-    }
 }
